fe4f30e401b43bf8d5e35aab36adc81a7d398188
sid-72 <siddharthdamor1@gmail.com>
2025-02-04 17:25:21 +0530

    Updated default route

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { UserProfileComponent } from './user-profile/user-profile.component';
 export const routes: Routes = [
   { path: 'feed/user/:userId', component: UserFeedComponent },
   { path: 'login', component: LoginComponent },
-  //{ path: '', redirectTo: '/feed', pathMatch: 'full' }, // Default route
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Default route
   //{ path: '**', redirectTo: '/feed' }, // Wildcard route for a 404 page
-  {path: 'users/:userId/profile', component: UserProfileComponent}  
+  {path: 'users/:userId/profile', component: UserProfileComponent}
 ];
